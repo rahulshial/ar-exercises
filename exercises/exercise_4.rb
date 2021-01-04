a33675d6b154084ee165e7930a138d41738739f5
Rahul <rahul.shial@gmail.com>
2021-01-04 21:34:48 +0000

    Code Clean-up

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -3,23 +3,38 @@ require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 
-puts "Exercise 4"
+puts "\nExercise 4"
 puts "----------"
 
 # Your code goes here ...
-Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
-Store.create(name: 'Whistler', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
-Store.create(name: 'Yaletown', annual_revenue: 460000, mens_apparel: true, womens_apparel: true)
+Store.create(
+  name: 'Surrey',
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true)
+
+Store.create(
+  name: 'Whistler',
+  annual_revenue: 190000,
+  mens_apparel: true,
+  womens_apparel: false)
+
+Store.create(
+  name: 'Yaletown', 
+  annual_revenue: 460000,
+  mens_apparel: true,
+  womens_apparel: true)
+
 puts "\nTotal Stores: #{Store.count}\n"
 
 @mens_store = Store.where(mens_apparel: true)
 
-puts "Men's store statistics"
+puts "\nMen's store statistics"
 @mens_store.each do |store|
-  puts "\n#{store.name}\'s annual revenue is #{store.annual_revenue}"
+  puts "#{store.name}\'s annual revenue is #{store.annual_revenue}"
 end
 
-puts "Women's store statistics"
+puts "\nWomen's store statistics"
 @womens_store = Store.where("womens_apparel = true and annual_revenue < 1000000")
 @womens_store.each do |store|
   puts "\n#{store.name}\'s annual revenue is #{store.annual_revenue}"
