e4a92c13b77c7f7b08d6f1f97bc86d149257525f
Rahul <rahul.shial@gmail.com>
2021-01-04 22:19:30 +0000

    ex6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -5,7 +5,42 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
-puts "Exercise 6"
+puts "\nExercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.employees.create(
+  first_name: "Rahul",
+  last_name: "Shial",
+  hourly_rate: 75)
+
+@store1.employees.create(
+  first_name: "Varsha",
+  last_name: "Shial",
+  hourly_rate: 75)
+
+@store1.employees.create(
+  first_name: "Mitra",
+  last_name: "Shial",
+  hourly_rate: 75)
+
+@store2.employees.create(
+  first_name: "Urmi",
+  last_name: "Shial",
+  hourly_rate: 75)
+
+@store2.employees.create(
+  first_name: "Jamai Babu",
+  last_name: "Shial",
+  hourly_rate: 75)
+
+@store2.employees.create(
+  first_name: "Naukar",
+  last_name: "Shial",
+  hourly_rate: 75)
+    
+    
